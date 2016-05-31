8f720e1396389238f483d4923e0e7ca19f6b0217
jb007em <eric.massiaux@gmail.com>
2016-05-31 09:37:16 +0200

    maj position orientation

--- src/Afpa/BattleGameBundle/Model/Board.php
+++ src/Afpa/BattleGameBundle/Model/Board.php
@@ -20,15 +20,44 @@ class Board {
 
 
             for ($col = 0; $col < 10; $col++) {
-                $this->boardShoot[$ligne][$col] = 'X';
-                $this->boardPieces[$ligne][$col] = 'X';
+                $this->boardShoot[$ligne][$col] = '';
+                $this->boardPieces[$ligne][$col] = '';
             }
         }
 
+        // Placement des pièces auto
+        $this->initPiecesAuto();
+    }
+
+    public function initPiecesAuto() {
         // initialisation des pieces
-        /* $oCav1 = new Cavalier($x, $y, $orientation);
-          $oCav2 = new Cavalier($x, $y, $orientation);
+        $oPiece = new Cavalier(Piece::randomOrientation());
+
+        // debug
+        print_r($oPiece);
+        print_r($oPiece::SIZE);
+
+        do {
+            $aPos = self::randomCoords();
+        } while (!$this->caseEstVide($aPos) && !$this->placementEstPossible($aPos, $oPiece));
+
+        $oPiece->setXY($aPos[0], $aPos[1]);
+
+        // debug
+        print_r($aPos);
+
+        /*  $oCav2 = new Cavalier($x, $y, $orientation);
           $oCav3 = new Cavalier($x, $y, $orientation); */
+    }
+
+    public function caseEstVide($aPos) {
+        // Est-ce que la case est vide
+        return TRUE;
+    }
+
+    public function placementEstPossible($aPos, $oPiece) {
+        // Est-ce les cases nécessaires sont vides et dans le plateau
+        return TRUE;
     }
 
     public function getBoardPieces() {
@@ -53,6 +82,10 @@ class Board {
 
     public function setPlayerTurn($playerTurn) {
         $this->playerTurn = $playerTurn;
+    }
+
+    public static function randomCoords() {
+        return array(rand(0, 9), rand(0, 9));
     }
 
 }
--- src/Afpa/BattleGameBundle/Model/Piece.php
+++ src/Afpa/BattleGameBundle/Model/Piece.php
@@ -6,15 +6,24 @@ abstract class Piece {
 
     const PICTURE = NULL;
     const SIZE = NULL;
+    const ORIENT_H = 0;
+    const ORIENT_V = 1;
 
     protected $posX;
     protected $posY;
     protected $orientation;
 
-    public function __construct($x, $y, $orientation) {
+    public function __construct($orientation) {
+        $this->orientation = $orientation;
+    }
+
+    public function setXY($x, $y) {
         $this->posX = $x;
         $this->posY = $y;
-        $this->orientation = $orientation;
+    }
+
+    public static function randomOrientation() {
+        return rand(0, 1);
     }
 
 }
